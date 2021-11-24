94db37d73ecb2c450094aaec06fecb85fca225d4
Tyler Funk <tcfunk25@gmail.com>
2021-11-24 12:21:06 -0600

    ranged controller fires a slightly different projectile

--- Assets/Scripts/Client/NetworkRangedAvatarController.cs
+++ Assets/Scripts/Client/NetworkRangedAvatarController.cs
@@ -1,8 +1,19 @@
-﻿using UnityEngine;
+﻿using Mirror;
+using UnityEngine;
 
 namespace Client
 {
     public class NetworkRangedAvatarController : NetworkAvatarController
     {
+        [SerializeField] private GameObject projectile;
+        [SerializeField] private Transform fireFrom;
+        
+        public override void Attack()
+        {
+            // Spawn magic projectile
+            var projectileDirection = Quaternion.LookRotation(transform.forward, Vector3.up);
+            var rangedProjectile = Instantiate(projectile, fireFrom.position, projectileDirection);
+            NetworkServer.Spawn(rangedProjectile);
+        }
     }
 }
