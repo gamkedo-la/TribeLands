4a3a7ba5dd90f930fdf72df007fc229c8383a705
Tyler Funk <tcfunk25@gmail.com>
2021-12-11 00:26:02 -0600

    enemies detect nearby players, choose target, path towards that target

--- Assets/Scripts/Client/Enemy.cs
+++ Assets/Scripts/Client/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class Enemy : NetworkBehaviour
@@ -16,6 +17,18 @@ public class Enemy : NetworkBehaviour
     
     [SyncVar]
     public float health;
+
+
+    public GameObject target;
+    public NavMeshAgent navAgent;
+
+    [SerializeField] private float detectionRadius = 10f;
+    [SerializeField] private float breakFollowDistance = 15f;
+    [SerializeField] private float avatarCheckInterval = 0.5f;
+    [SerializeField] private float attackRange = 1.0f;
+    [SerializeField] private LayerMask avatarLayerMask;
+    private float timeSinceLastCheck = 0f;
+    private Collider[] nearbyAvatars;
     
     // Start is called before the first frame update
     void Start()
@@ -23,6 +36,42 @@ public class Enemy : NetworkBehaviour
         health = maxHealth;
         healthBarCanvas = healthBar.transform.parent;
         mainCamera = Camera.main;
+        navAgent = GetComponentInChildren<NavMeshAgent>();
+    }
+
+    public override void OnStartServer()
+    {
+        nearbyAvatars = new Collider[3];
+    }
+
+    private void Update()
+    {
+        if (!isServer) return;
+
+        timeSinceLastCheck += Time.deltaTime;
+        if (target == null && timeSinceLastCheck > avatarCheckInterval)
+        {
+            FindTarget();
+        }
+        
+        if (target != null)
+        {
+            if (navAgent.hasPath)
+            {
+                // Check if target has gotten too far away.
+                if (Vector3.Distance(transform.position, target.transform.position) >= breakFollowDistance)
+                {
+                    RemoveTarget();
+                    return;
+                }
+                
+                Debug.Log($"{gameObject.name} navAgent path status: {navAgent.path.status.ToString()}");
+            }
+            else
+            {
+                Debug.LogError($"navAgent has target {target.gameObject.name}, but no valid path was found");
+            }
+        }
     }
 
     // Update is called once per frame
@@ -43,8 +92,31 @@ public class Enemy : NetworkBehaviour
     }
 
     [Server]
+    private void FindTarget()
+    {
+        var numColliders = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, nearbyAvatars, avatarLayerMask);
+
+        // Just get the first, if there is one.
+        if (numColliders > 0)
+        {
+            target = nearbyAvatars[0].gameObject;
+        
+            var path = new NavMeshPath();
+            navAgent.CalculatePath(target.transform.position, path);
+            navAgent.SetPath(path);
+        }
+    }
+
+    [Server]
     private void DestroySelf()
     {
         NetworkServer.Destroy(gameObject);
+    }
+
+    [Server]
+    private void RemoveTarget()
+    {
+        target = null;
+        navAgent.ResetPath();
     }
 }
