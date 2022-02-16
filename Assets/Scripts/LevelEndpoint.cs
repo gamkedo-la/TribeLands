7fb93a85645f5c2d9de6e6a72bd82c160b03593c
Tyler Funk <tcfunk25@gmail.com>
2022-02-16 15:52:59 -0600

    scene cleanup and ForEveryScene prefab added

--- Assets/Scripts/LevelEndpoint.cs
+++ Assets/Scripts/LevelEndpoint.cs
@@ -1,25 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem;
 
 public class LevelEndpoint : MonoBehaviour
 {
     public string NextScene;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    // public void LoadNextScene(InputAction.CallbackContext ctx)
     public void LoadNextScene()
     {
         SceneManager.LoadScene(NextScene);
@@ -28,9 +13,9 @@ public class LevelEndpoint : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         var avatar = other.GetComponent<NetworkAvatar>();
-        if (avatar != null) {
+        if (avatar != null)
+        {
             LoadNextScene();
-            Debug.Log($"triggered by {other.name}, load next level");
         }
     }
 
