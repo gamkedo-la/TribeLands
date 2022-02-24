0e772dd965bec604325c6b68cb7fffdcb07eb8a9
Tyler Funk <tcfunk25@gmail.com>
2022-02-24 01:17:27 -0600

    can now host game from main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,17 +8,28 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button creditsButton;
     [SerializeField] private Button exitButton;
 
+    [SerializeField] private GameObject playPanel;
+
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button joinButton;
+
     void Start()
     {
         playButton.onClick.AddListener(OnPlayButtonPressed);
         settingsButton.onClick.AddListener(OnSettingsButtonPressed);
         creditsButton.onClick.AddListener(OnCreditsButtonPressed);
         exitButton.onClick.AddListener(OnExitButtonPressed);
+        
+        hostButton.onClick.AddListener(OnHostButtonPressed);
+        joinButton.onClick.AddListener(OnJoinButtonPressed);
+        
+        playPanel.SetActive(false);
     }
 
     void OnPlayButtonPressed()
     {
         Debug.Log("Play button pressed");
+        playPanel.SetActive(true);
     }
 
     void OnSettingsButtonPressed()
@@ -38,5 +49,21 @@ public class MainMenu : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    void OnHostButtonPressed()
+    {
+        GroupNetworkManager.singleton.StartHost();
+        GroupNetworkManager.singleton.ServerChangeScene("Dungeon");
+    }
+
+    void OnJoinButtonPressed()
+    {
+        var ipAddress = "localhost";
+        GroupNetworkManager.singleton.networkAddress = ipAddress;
+        GroupNetworkManager.singleton.StartClient();
+        // Debug.Log($"Join game at {ipAddress}");
+        // 1. Check that an IP address was provided
+        // 2. Join game at that address
     }
 }
