c684bdd96dee60786e41ff8e01cba1f861dc8ee5
Tyler Funk <tcfunk25@gmail.com>
2022-03-15 15:12:29 -0500

    combat idle, attack, and power attack animations for tua

--- Assets/Scripts/Client/GameManager.cs
+++ Assets/Scripts/Client/GameManager.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 // Manages events within the local / client environment
 // Anything that doesn't need to be sent over the network
--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LevelData", menuName = "Data/LevelData")]
