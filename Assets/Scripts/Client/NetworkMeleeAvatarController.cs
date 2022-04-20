e12f66468b70cb02ed85a62798cec8d2e8116223
Tyler Funk <tcfunk25@gmail.com>
2022-04-19 23:38:18 -0500

    add hippo attack and power attack animations

--- Assets/Scripts/Client/NetworkMeleeAvatarController.cs
+++ Assets/Scripts/Client/NetworkMeleeAvatarController.cs
@@ -5,5 +5,9 @@ namespace Client
 {
     public class NetworkMeleeAvatarController : NetworkAvatarController
     {
+        public override void BeginAttack()
+        {
+            // nothing to do here
+        }
     }
 }
