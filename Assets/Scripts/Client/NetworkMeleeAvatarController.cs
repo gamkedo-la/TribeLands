6dabdc5eb2b6c2b3243334979f0b43dd76eb26b4
Tyler Funk <tcfunk25@gmail.com>
2022-04-20 13:11:22 -0500

    add field to assign melee power attack vfx

--- Assets/Scripts/Client/NetworkMeleeAvatarController.cs
+++ Assets/Scripts/Client/NetworkMeleeAvatarController.cs
@@ -1,13 +1,27 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using Mirror;
+using UnityEngine;
 
 namespace Client
 {
     public class NetworkMeleeAvatarController : NetworkAvatarController
     {
+        [SerializeField] private GameObject powerAttackEffect;
+        
         public override void BeginAttack()
         {
             // nothing to do here
+        }
+
+        [Command]
+        public override void CmdPowerAttack()
+        {
+            Debug.Log("melee power attack");
+            if (powerAttackEffect == null) return;
+            
+            var effectRotation = Quaternion.identity;
+            var effect = Instantiate(powerAttackEffect, transform.position, effectRotation);
+            NetworkServer.Spawn(effect);
+            Destroy(effect, 1f);
         }
     }
 }
